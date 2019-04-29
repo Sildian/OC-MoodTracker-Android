397539a88afb00add1205fc22f19812d742cf098
Nicolas MAYER-WEILER <sildian@MacBook-Air-de-Nicolas.local>
2019-04-29 16:00:57 +0200

    Added the ressources

--- app/src/main/java/com/sildian/moodtracker/controller/MainActivity.java
+++ app/src/main/java/com/sildian/moodtracker/controller/MainActivity.java
@@ -1,7 +1,9 @@
-package com.sildian.moodtracker;
+package com.sildian.moodtracker.controller;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.sildian.moodtracker.R;
 
 public class MainActivity extends AppCompatActivity {
 
